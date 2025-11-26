94052820971145bf02b8dd647bf4adf5d7006878
Umar hayat <umarhayatlohar0000@gmail.com>
2025-11-26 17:48:51 +0500

    Fix deployment #1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning/" : "",
   output: "export",
   images: {
     unoptimized: true,
