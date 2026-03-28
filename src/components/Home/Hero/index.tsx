487d132c27a47241e4477575812e0aa48c301189
Umar hayat <umar.hayat.2k05@gmail.com>
2026-03-28 23:00:06 +0500

    :)

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -121,7 +121,7 @@ export default function Hero() {
                   className="w-full pl-11 pr-28 py-4 text-sm text-gray-800 placeholder:text-gray-400 focus:outline-none bg-transparent"
                 />
                 <button className="absolute right-2 px-4 py-2 rounded-xl text-xs font-semibold text-white bg-gradient-to-r from-blue-600 to-green-400 hover:opacity-90 transition">
-                  Search
+                  Search :)
                 </button>
               </div>
             </div>
