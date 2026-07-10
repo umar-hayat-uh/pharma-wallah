7e8602ec83b0ab699d6d985e18c385f3729dd62e
shaani <shayanhusein@gmail.com>
2026-07-10 21:01:39 +0500

    fixed-signup

--- src/app/(site)/signup/page.tsx
+++ src/app/(site)/signup/page.tsx
@@ -51,7 +51,7 @@ export default function SignUpPage() {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: `${window.location.origin}/dashboard`,
+                redirectTo: `${window.location.origin}/api/auth/callback`,
             },
         });
         if (error) setError(error.message);
